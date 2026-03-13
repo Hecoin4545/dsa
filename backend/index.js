83ad65563a44d416e4b0e47dafde22ce6dc2c8e4
Hecoin4545 <hecoin45@gmail.com>
2026-03-13 17:55:18 +0530

    aa

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true,
+    origin: true,
+    credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
